5691d6eb9f4713120170d9712765842644e5a949
Bill McGair <bill@mcgair.com>
2016-02-22 14:22:56 -0800

    fix nat rule order

--- launchvpc-Oregon.sh
+++ launchvpc-Oregon.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #### Increment BASTION_ID to launch new box!
-export BASTION_ID="13"
+export BASTION_ID="02"
 export AWS_REGION_CODE="us-west-2"
 export AWS_REGION_NAME="Oregon"
 export VPC_BASE="10.10"
 export KEYPAIR="xaas-or"
 export AMI="ami-9abea4fb"
+export MGMT_IP="24.21.200.124/32"
 
 
 /usr/local/bin/ansible-playbook -i contribl/ec2.py ./vpc-stage1.yml --vault-password-file ~/.vault_pass.txt 
