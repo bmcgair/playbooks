11de31cae545369542b14d5dba005ab241b2742c
Bill McGair <bill@mcgair.com>
2016-02-24 22:09:50 -0800

    multi region working

--- launchvpc-nova.sh
+++ launchvpc-nova.sh
@@ -2,16 +2,17 @@
 
 #### Increment BASTION_ID to launch new box!
 ### Otherwise script will behave idempotently
-export BASTION_IDEMPOTENT_ID="74"
+export BASTION_IDEMPOTENT_ID="75"
 
-export REGION="us-west-2"
-export ZONEA="us-west-2a"
-export ZONEB="us-west-2b"
-export ZONEC="us-west-2c"
+export REGION="us-east-1"
+export ZONEA="us-east-1a"
+export ZONEB="us-east-1b"
+export ZONEC="us-east-1e"
 
-export VPC_BASE="10.10"
-export KEYPAIR="xaas-or"
+export VPC_BASE="10.20"
+export KEYPAIR="xaas-va"
 export MGMT_IP="`curl -s icanhazip.com`/32"
+
 
 export BASTION_IMAGE_NAME="ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"
 export BASTION_IMAGE_OWNER="099720109477"
