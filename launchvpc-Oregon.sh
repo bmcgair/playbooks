61cd2e5e78eea8aadddb6c23e10a9059e566537e
Bill McGair <bill@mcgair.com>
2016-02-26 10:43:57 -0800

    first crack at ecs cluster

--- launchvpc-Oregon.sh
+++ launchvpc-Oregon.sh
@@ -16,6 +16,10 @@ export MGMT_IP="`curl -s icanhazip.com`/32"
 export BASTION_IMAGE_NAME="ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"
 export BASTION_IMAGE_OWNER="099720109477"
 
+export ECS_IMAGE_NAME="ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"
+export ECS_IMAGE_OWNER="099720109477"
+
 /usr/local/bin/ansible-playbook -i contribl/ec2.py ./vpc-stage1.yml
 /usr/local/bin/ansible-playbook -i contribl/ec2.py ./vpc-stage2.yml
 /usr/local/bin/ansible-playbook -i contribl/ec2.py ./vpc-stage3.yml
+/usr/local/bin/ansible-playbook -i contribl/ec2.py ./vpc-ecs-cluster.yml
